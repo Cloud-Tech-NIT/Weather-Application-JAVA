8f9d3d76963bffc1288b9bfc962e9b62529535bb
ezza-abdullah1 <ezza.abdullahh@gmail.com>
2024-03-27 16:15:22 +0500

    data passed to duifiller dile

--- Src/BusinessLogic/DUIFiller.java
+++ Src/BusinessLogic/DUIFiller.java
@@ -1,6 +1,16 @@
 package Src.BusinessLogic;
 
 public class DUIFiller {
+
+  public void setLatLong(double latitude, double longitude) {
+    // Implement the logic to use latitude and longitude as needed
+    System.out.println("Latitude: " + latitude + ", Longitude: " + longitude);
+  }
+
+  public void setcity(String city) {
+    // Implement the logic to use latitude and longitude as needed
+    System.out.println("city name: " + city);
+  }
   // This is the class for implementation of Desktop UI
   // Make the objects of your class of desktop UI in this class
   // Make functions to pass in the lat,long / city name along with the required
