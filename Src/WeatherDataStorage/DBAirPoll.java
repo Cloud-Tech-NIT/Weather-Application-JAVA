12fca62332c0d5f70723173084185086504bd3af
Tehreem Tahir <tehreem.4861@gmail.com>
2024-03-31 10:27:22 +0500

    Updated DBairpoll

--- Src/WeatherDataStorage/DBAirPoll.java
+++ Src/WeatherDataStorage/DBAirPoll.java
@@ -1,6 +1,4 @@
 package Src.WeatherDataStorage;
-
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -16,7 +14,6 @@ import java.time.Instant;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
-
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
@@ -259,5 +256,8 @@ public class DBAirPoll {
 
         // Example API call
         fetcher.APIcall(40.7128, -74.0060); // Example latitude and longitude
+        
+        // Message to check if the code runs properly
+        System.out.println("Code executed successfully!");
     }
 }
