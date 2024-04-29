2e39a385b440168dfd82ae287a9ff2173988f6ce
Tehreem Tahir <tehreem.4861@gmail.com>
2024-04-29 20:14:45 +0500

    separated schemas

---
+++ Src/WeatherDataStorage/schemas/5DayForecast(schema).sql
@@ -0,0 +1,13 @@
+CREATE TABLE WeatherForecast (
+    loc_id INT PRIMARY KEY,
+    city_name VARCHAR(255),
+    latitude FLOAT,
+    longitude FLOAT,
+    date DATE,
+    temp FLOAT,
+    temp_min FLOAT,
+    temp_max FLOAT,
+    pressure FLOAT,
+    humidity FLOAT,
+    city_coordinates VARCHAR(255)
+);
