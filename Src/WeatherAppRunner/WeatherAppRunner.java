787ff11523fff179cb5ebb99f58cf869d6255776
Manal-Bukhari <manalbukhari2233@gmail.com>
2024-05-01 21:04:50 +0500

    weatherRunner for TUI

--- Src/WeatherAppRunner/WeatherAppRunner.java
+++ Src/WeatherAppRunner/WeatherAppRunner.java
@@ -1,5 +1,9 @@
 package Src.WeatherAppRunner;
 
+import Src.AppUI.TerminalUI;
+import Src.BusinessLogic.TerminalUI.TUI_DB;
+import Src.BusinessLogic.TerminalUI.TUI_Txt;
+
 public class WeatherAppRunner {
 
   WeatherAppRunner() {
@@ -9,10 +13,16 @@ public class WeatherAppRunner {
 
   public void RunTUI_TXT() {
     DBType = "txt";
+    TUI_Txt  txt = new TUI_Txt();
+    TerminalUI terminal = new TerminalUI(txt);
+    terminal.RunF();
   }
 
   public void RunTUI_SQL() {
     DBType = "SQL";
+    TUI_DB db = new TUI_DB();
+    TerminalUI terminal = new TerminalUI(db);
+    terminal.RunF();
   }
 
   public void RunDUI_TXT() {
@@ -27,9 +37,9 @@ public class WeatherAppRunner {
 
     WeatherAppRunner AppRunner = new WeatherAppRunner();
 
-    AppRunner.RunDUI_SQL();
-    AppRunner.RunDUI_TXT();
+   AppRunner.RunDUI_SQL();
+   AppRunner.RunDUI_TXT();
     AppRunner.RunTUI_TXT();
-    AppRunner.RunTUI_SQL();
+   AppRunner.RunTUI_SQL();
   }
 }
